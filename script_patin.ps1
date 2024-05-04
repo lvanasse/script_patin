94a020518b17bff9bb6a120b0696e266395c3fa5
Ludovic Vanasse <ludovicvanasse@gmail.com>
2024-05-04 19:53:38 -0400

    Implement missing feature for first user test
    Add argument -dl to provide drive letters. Also add the modern
    Powershell parallelism. And some other improvement. This makes the
    script pretty much ready to be tested with the user.

--- script_patin.ps1
+++ script_patin.ps1
@@ -1,6 +1,6 @@
 param (
     [string]$fileArgument = $null,
-    [string[]]$driveLetters = @(),
+    [string]$dl = $null,
     [switch]$help
 )
 
@@ -8,52 +8,66 @@ if ($help) {
     Write-Host "This script format multiple drive and copy a file onto all of them."
     Write-Host "Arguments:"
     Write-Host "-f : Specify the file you want to copy."
-	Write-Host "-dl : Specify a list of drive letters."
-	Write-Host "-help : Print this help message"
+    Write-Host "-dl : Specify a list of drive letters."
+    Write-Host "-help : Print this help message"
     exit
 }
 
 if (-not $fileArgument) {
     # Prompt user to enter file name
-    $fileName = Read-Host "Enter the file name"
-    $fileArgument = (Resolve-Path $fileName).Path
+    $fileArgument = Read-Host "Enter the file name"
 }
 
+# Resolve the absolute path of the file
+$fileAbsolutePath = (Resolve-Path $fileArgument).Path
+
 # Check if file exists
-if (-not (Test-Path $fileArgument)) {
-    Write-Host "File not found: $fileArgument"
+if (-not (Test-Path $fileAbsolutePath)) {
+    Write-Host "File not found: $fileAbsolutePath"
     # Quit the script
     exit
 }
 
-# List volumes with ExFAT or FAT file systems
-$driveInfo = Get-Volume | Where-Object { $_.FileSystemType -in 'FAT32', 'exFAT', 'Unknown' -and $_.DriveLetter } | Select-Object DriveLetter, FileSystemType, @{Name="Size(MB)";Expression={[math]::Round($_.Size / 1MB, 2)}}
+if (-not [string]::IsNullOrEmpty($dl)) {
+    $dl = $dl -replace ' ', ','
+    # Convert comma-separated list to array of drive letters
+    $driveLetters = $dl -split ","
 
-# Display drive information
-Write-Host "Available drives:"
-$driveInfo | Format-Table -AutoSize
-
-if ($driveLetters.Count -eq 0) {
+    # Check if each drive letter exists
+    foreach ($driveLetter in $driveLetters) {
+        $test_driverLetter = $driveLetter + ":\"
+        if (-not (Test-Path $test_driverLetter)) {
+            Write-Host "Drive $driveLetter does not exist. Exiting script. Please provid a driver letter that exist"
+            exit
+        }
+    }
+}
+else {
+    # List volumes with ExFAT or FAT file systems
+    $driveInfo = Get-Volume | Where-Object { $_.FileSystemType -in 'FAT32', 'exFAT', 'Unknown' -and $_.DriveLetter } | Select-Object DriveLetter, FileSystemType, @{Name = "Size(MB)"; Expression = { [math]::Round($_.Size / 1MB, 2) } }
+    # Display drive information
+    Write-Host "Available drives:"
+    $driveInfo | Format-Table -AutoSize
+    # Prompt user to enter drive letters
     $driveLetters = (Read-Host "Enter drive letters separated by comma (e.g., C, D)").Split(',')
 }
 
-Write-Host $fileArgument
+Write-Host "Drive letters provided: $($driveLetters -join ', ')"
 
-# Start background jobs to format each drive to ExFAT and copy the file
-foreach ($driveLetter in $driveLetters) {
-    Start-Job -ScriptBlock {
-        param ($dl, $file)
-        # $formatOutput = Format-Volume -DriveLetter $dl -FileSystem exFAT -Confirm:$false
-        # Write-Host "Output of formatting drive $dl to ExFAT:"
-        # $formatOutput
-        Write-Host $file 
-        # Copy-Item -Path $file -Destination "$dl\"
-        # Write-Host "File copied to drive $dl"
-    } -ArgumentList $driveLetter, $fileArgument | Out-Null
+# Start parallel jobs to format each drive to ExFAT and copy the file
+$driveLetters | ForEach-Object -Parallel {
+    $driveLetter = $_
+
+    # Format drive to ExFAT
+    Format-Volume -DriveLetter $driveLetter -FileSystem exFAT -Confirm:$false *> $null
+    Write-Host "Drive $driveLetter formatted to ExFAT"
+
+    # Copy file to drive
+    $destinationPath = $driveLetter + ":\"
+
+    Copy-Item -Path $using:fileAbsolutePath -Destination $destinationPath -Force
+    Write-Host "File copied to drive $driveLetter"
 }
 
 # Wait for all jobs to complete
 Get-Job | Wait-Job | Out-Null
-
-# Receive output from each job
-Get-Job | Receive-Job
